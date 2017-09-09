339a586c230a2bc793cef4887633d43ef81c8b4f
J Fixby <github@jfixby.com>
2017-09-09 11:09:21 +0200

    implement

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,54 @@
 // Copyright (c) 2017, JCode. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
-
-import 'package:export_files_generator/export_files_generator.dart' as export_files_generator;
+import 'package:export_files_generator/generator.dart';
+import 'package:scarabei/api/debug/debug.dart';
+import 'package:scarabei/api/error/err.dart';
+import 'package:scarabei/api/files/file.dart';
+import 'package:scarabei/api/files/local_file_system.dart';
+import 'package:scarabei/api/log/logger.dart';
+import 'package:scarabei/api/strings/strings.dart';
+import 'package:scarabei/api/sys/execution_mode.dart';
+import 'package:scarabei/api/sys/settings/system_settings.dart';
+import 'package:scarabei/api/sys/sys.dart';
+import 'package:scarabei/api/utils/utils.dart';
+import 'package:scarabei_reyer/red-desktop/sys/red_desktop_system.dart';
+import 'package:scarabei_reyer/red-desktop/sys/settings/red_system_settings.dart';
+import 'package:scarabei_reyer/red/debug/red_debug.dart';
+import 'package:scarabei_reyer/red/error/red_error.dart';
+import 'package:scarabei_reyer/red/files/posix/unix_file_system.dart';
+import 'package:scarabei_reyer/red/files/win/win_file_system.dart';
+import 'package:scarabei_reyer/red/log/simple_logger.dart';
+import 'package:scarabei_reyer/red/strings/red_strings.dart';
+import 'package:scarabei_reyer/red/utils/red_utils.dart';
 
 main(List<String> arguments) {
-  print('Hello world: ${export_files_generator.calculate()}!');
+//  print('Hello world: ${export_files_generator.calculate()}!');
+
+  _setup();
+
+  String targetFolderPath = arguments[0];
+  File root = LocalFileSystem.newLocalFile(targetFolderPath);
+  if (!root.exists()) {
+    Err.reportError("Folder not found: $root");
+    Sys.exit();
+  }
+
+  ExportFilesGenerator.generate(root);
+}
+
+void _setup() {
+  L.installComponent(new SimpleLogger());
+  Err.installComponent(new RedError());
+  Debug.installComponent(new RedDebug());
+  Utils.installComponent(new RedUtils());
+  Strings.installComponent(new RedStrings());
+  Sys.installComponent(new DesktopSystem());
+  SystemSettings.installComponent(new RedSystemSettings());
+
+  if (Sys.isUnix()) {
+    LocalFileSystem.installComponent(new UnixFileSystem());
+  } else {
+    LocalFileSystem.installComponent(new WinFileSystem());
+  }
+  SystemSettings.setExecutionMode(ExecutionMode.DEMO);
 }
---
+++ lib/generator.dart
@@ -0,0 +1,62 @@
+import 'package:scarabei/api/files/file.dart';
+import 'package:scarabei/api/files/files_list.dart';
+import 'package:scarabei/api/log/logger.dart';
+import 'package:scarabei/api/path/relative_path.dart';
+import 'package:scarabei/api/utils/utils.dart';
+
+class ExportFilesGenerator {
+  static final String PUBSEC = "pubspec.yaml";
+  static final String N = "\n";
+  static final String R = "\r";
+
+  static void generate(File root) {
+    FileFilter pubsecFilter = (file) => file.getName() == "pubsec.yaml";
+
+    FilesList pubsecFiles = root.listAllChildren(fileFilter: pubsecFilter);
+
+    for (File pubsec in pubsecFiles.toList()) {
+      processProject(pubsec);
+    }
+  }
+
+  static void processProject(File pubsec) {
+    File project_root = pubsec.parent();
+    String pubsecData = pubsec.readString();
+    final String name = pubsecData.split(N)[0].replaceAll("name: ", "").replaceAll(N, "").replaceAll(R, "");
+    L.d("project", project_root);
+    L.d("   name", name);
+    L.d("       ", pubsec);
+
+    final File lib = project_root.child("lib");
+    final File exportFile = lib.child(name + ".dart");
+
+    FileFilter dartFilter = (file) => file.extensionIs("dart");
+
+    final FilesList dartFiles = lib.listAllChildren(fileFilter: dartFilter);
+    final List<String> exportFileContent = [];
+    final RelativePath prefix = project_root.getAbsoluteFilePath().getRelativePath();
+    for (final File dart in dartFiles.toList()) {
+      final RelativePath dart_prefix = dart.getAbsoluteFilePath().getRelativePath();
+      List<String> postfix = dart_prefix.steps();
+      postfix = postfix.sublist(0, prefix.size() + 1); //lib
+//    postfix = postfix.splitAt(prefix.size() + 1);// lib
+      final RelativePath split = Utils.newRelativePath(path_steps: postfix);
+// import 'package:van_mobile_api/van_mobile_api.dart'
+      final String fileName = split.getLastStep();
+      if (fileName == ("main.dart")) {
+        continue;
+      }
+      if (fileName == (exportFile.getName())) {
+        continue;
+      }
+      exportFileContent.add("export 'package:" + name + RelativePath.SEPARATOR + split.toString() + "';");
+      exportFileContent.add(N);
+
+// L.d("split", split);
+    }
+// L.d(exportFileContent);
+    L.d("writing", exportFile);
+    exportFile.writeString(exportFileContent.join(""));
+    L.d("");
+  }
+}
